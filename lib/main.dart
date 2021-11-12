c030d606e832de719d193bd22647fcc335e091b7
labib muhajir <labib.muhajir@metroindonesia.com>
2021-11-12 14:45:25 +0700

    bloc/popular-movie implement bloc in PopularMoviePage

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/domain/entities/id_and_data_type.dart';
 import 'package:ditonton/presentation/bloc/movie_detail/movie_detail_bloc.dart';
 import 'package:ditonton/presentation/bloc/movie_search/movie_search_bloc.dart';
+import 'package:ditonton/presentation/bloc/popular_movie/popular_movie_bloc.dart';
 import 'package:ditonton/presentation/bloc/popular_tv_series/popular_tv_series_bloc.dart';
 import 'package:ditonton/presentation/bloc/tv_detail/tv_detail_bloc.dart';
 import 'package:ditonton/presentation/bloc/tv_searies_search/tv_series_search_bloc.dart';
@@ -77,6 +78,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => di.locator<TvDetailBloc>()),
         BlocProvider(create: (_) => di.locator<MovieDetailBloc>()),
         BlocProvider(create: (_) => di.locator<PopularTvSeriesBloc>()),
+        BlocProvider(create: (_) => di.locator<PopularMovieBloc>()),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
---
+++ test/presentation/bloc/popular_movie_bloc_test.dart
@@ -0,0 +1,73 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/id_poster_title_overview.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/get_popular_movies.dart';
+import 'package:ditonton/presentation/bloc/popular_movie/popular_movie_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../provider/popular_movies_notifier_test.mocks.dart';
+
+void main() {
+  late GetPopularMovies getPopularMovies;
+  late PopularMovieBloc bloc;
+
+  setUp(() {
+    getPopularMovies = MockGetPopularMovies();
+    bloc = PopularMovieBloc(getPopularMovies);
+  });
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovieList = <Movie>[tMovie];
+  final expected = tMovieList.map((e) => IdPosterTitleOverview.fromMovie(e)).toList();
+
+  test('inital state should be initial', () {
+    expect(bloc.state, PopularMovieInitial());
+  });
+
+  blocTest('Should emit [Loading, HasData] when data is gotten succesful',
+    build: () {
+      when(getPopularMovies.execute())
+          .thenAnswer((realInvocation) async => Right(tMovieList));
+
+      return bloc;
+    },
+    act: (PopularMovieBloc bloc) => bloc.add(OnPopularMovieDataRequested()),
+    wait: const Duration(milliseconds: 500),
+    expect: () => [PopularMovieLoading(), PopularMovieSuccess(expected)],
+    verify: (PopularMovieBloc bloc) {
+      verify(getPopularMovies.execute());
+    });
+
+
+  blocTest('Should emit [Loading, Error] when data is gotten succesful',
+    build: () {
+      when(getPopularMovies.execute())
+          .thenAnswer((realInvocation) async => Left(ServerFailure()));
+
+      return bloc;
+    },
+    act: (PopularMovieBloc bloc) => bloc.add(OnPopularMovieDataRequested()),
+    wait: const Duration(milliseconds: 500),
+    expect: () => [PopularMovieLoading(), PopularMovieError('Server Failure', retry: () {})],
+    verify: (PopularMovieBloc bloc) {
+      verify(getPopularMovies.execute());
+    });
+}
